272ee6f913854d8f875ad4d998e97a509adc50e9
João Philipe <Joaopphilipessampaio@gmail.com>
2026-02-17 17:46:29 -0300

    Etapa 2 - Criado Model Cliente

--- CRUD/obj/Debug/net9.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net9.0/CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca7b76eab32d33436cd9f96987210adf7f122da")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
